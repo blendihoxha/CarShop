6c571d355afd6d863f6ad8e79585d0161daee3c0
blendihoxha <blendihooxhaa@gmail.com>
2023-04-06 10:49:26 -0400

    change header and service

--- CarShop/src/js/header.js
+++ CarShop/src/js/header.js
@@ -1,22 +1,14 @@
 
 import React from 'react';
 
-// function Header() {
-//   return (
-//     <header className='header'>
-//       <h1>BlendeDealership.pro</h1>
+ function Header() {
+   return (
+     <header className='header'>
+       <h1>BlendeDealership.pro</h1>
       
-//     </header>
-//   );
-// }
-function Header({ handleHomeClick }) {
-  return (
-    <header className='header'>
-      <h1>BlendeDealership.pro</h1>
-      
-    </header>
-  );
-}
+     </header>
+   );
+ }
 
 
 export default Header;
